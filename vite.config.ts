f04badbb2dcd673e90200cc0b90a827d51832921
Dmytro Barylo <bdpochta@gmail.com>
2023-11-02 15:22:30 +0100

    Initial setup

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { glob } from "glob"
+import * as path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    root: path.join(__dirname, "src"),
+    build: {
+      rollupOptions: {
+        input: glob.sync(path.resolve(__dirname, "src", "*.html")),
+      },
+      emptyOutDir: true,
+      outDir: path.resolve(__dirname, `dist-${mode}`),
+    },
+    plugins: [
+      preact(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: './assets/icons',
+            dest: '',
+          },
+          {
+            src: `./manifest/manifest-${mode}.json`,
+            dest: '.',
+            rename: 'manifest.json'
+          }
+        ]
+      })
+    ],
+  }
+})
